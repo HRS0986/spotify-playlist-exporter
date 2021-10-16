0fdd40144a6433fae2ee68aa08a7025bf4515a9d
Hirusha Fernando <heshanhfernando@gmail.com>
2021-10-16 07:18:08 +0530

    Added playlists information card

--- src/app/components/basic-layout/basic-layout.component.ts
+++ src/app/components/basic-layout/basic-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy  } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SpotifyService } from '../../services/spotify.service';
 import { SpotifyProfileData } from '../../types';
